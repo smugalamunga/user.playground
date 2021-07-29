807aea138fb4e2df8628e288ad6617650fd99a55
Jon Kurisu <jkurisu@gmail.com>
2021-07-29 07:31:55 -0700

    Added Stub Activity

---
+++ pkg/activities/activities.go
@@ -0,0 +1,15 @@
+package activities
+
+import "context"
+
+func CreateUserActivity(ctx context.Context) error {
+	return nil
+}
+
+func RemoveUserActivity(ctx context.Context) error {
+	return nil
+}
+
+func UpdateUserActivity(ctx context.Context) error {
+	return nil
+}
